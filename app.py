13f1395f1171040f70296208065c2d10ca87c668
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-09-13 13:15:23 +0530

    attempt 6.2 2 make it work

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from waitress import serve
-from app import app
 
 # port = int(os.environ.get("PORT", 8080))
 # serve(app, host='0.0.0.0', port=port)
